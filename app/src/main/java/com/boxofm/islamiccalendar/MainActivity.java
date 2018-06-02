a5b878a2e2b7be84161cc5569b6bc36dcc18163e
Hasnain Baxamoosa <hbaxamoosa@gmail.com>
2018-06-02 13:08:31 -0500

    Adding check to see if Islamic Month and Day are the same, as the selection changes.

--- app/src/main/java/com/boxofm/islamiccalendar/MainActivity.java
+++ app/src/main/java/com/boxofm/islamiccalendar/MainActivity.java
@@ -2,6 +2,7 @@ package com.boxofm.islamiccalendar;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -14,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -39,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                returnDate(year, monthOfYear + 1, dayOfMonth);
+                returnDate(year, monthOfYear, dayOfMonth);
             }
         });
 
@@ -59,12 +61,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void returnDate(int year, int monthOfYear, int dayOfMonth) {
         double jd_date = Utility.gregorian_to_jd(year, monthOfYear, dayOfMonth);
-        Log.v(TAG, "JD: " + jd_date);
+        // Log.v(TAG, "JD: " + jd_date);
         double[] islamic_date = Utility.jd_to_islamic(jd_date);
         String[] islamicMonths = getResources().getStringArray(R.array.hijri_months);
-        Log.v(TAG, "Islamic: " + "year is " + islamic_date[0] + " month is " + islamicMonths[(int) islamic_date[1]] + " day is " + islamic_date[2]);
+        // Log.v(TAG, "Islamic: " + "year is " + islamic_date[0] + " month is " + islamicMonths[(int) islamic_date[1]] + " day is " + islamic_date[2]);
         textView.setText(new StringBuilder().append("JD: ").append(dayOfMonth).append("/")
                 .append(monthOfYear).append("/").append(year));
+        isItToday(jd_date);
+    }
+
+    private void isItToday(double jd_date) {
+        // first get today's islamic day and month
+        Calendar currentDate = Calendar.getInstance();
+        double currentJdDate = Utility.gregorian_to_jd(currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DAY_OF_MONTH));
+        Log.v(TAG, "currentJdDate: " + currentJdDate);
+        double[] currentIslamicDate = Utility.jd_to_islamic(currentJdDate);
+        String[] islamicMonths = getResources().getStringArray(R.array.hijri_months);
+
+        // today's Islamic date
+        Log.v(TAG, "currentIslamicDate: " + "year is " + currentIslamicDate[0] + " month is " + islamicMonths[(int) currentIslamicDate[1]] + " day is " + currentIslamicDate[2]);
+
+        // selected Islamic date
+        double[] selectedIsalmicDate = Utility.jd_to_islamic(jd_date);
+        Log.v(TAG, "selectedIsalmicDate: " + "year is " + selectedIsalmicDate[0] + " month is " + islamicMonths[(int) selectedIsalmicDate[1]] + " day is " + selectedIsalmicDate[2]);
+
+        if ((currentIslamicDate[1] == selectedIsalmicDate[1]) && (currentIslamicDate[2] == selectedIsalmicDate[2])) {
+
+            Context context = getApplicationContext();
+            CharSequence text = "Islamic Month and Day are the same!";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
     @Override
